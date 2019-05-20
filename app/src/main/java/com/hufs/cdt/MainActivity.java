bcc1d677bd596bb906ac75e419fe0e084fd3bf17
kwonhwan <juoo2@nate.com>
2019-05-20 19:21:01 +0900

    Initial commit

---
+++ app/src/main/java/com/hufs/cdt/MainActivity.java
@@ -0,0 +1,65 @@
+package com.hufs.cdt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button post_btn2 = (Button)findViewById(R.id.post_btn1);
+        Button mypage_btn = (Button)findViewById(R.id.mypage_btn);
+        Button search_btn = (Button)findViewById(R.id.search_btn);
+        Button msg_btn = (Button)findViewById(R.id.msg_btn);
+
+
+        post_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        Post.class);
+                startActivity(intent);
+            }
+        });
+
+        mypage_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        Mypage.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        Search.class);
+                startActivity(intent);
+            }
+        });
+
+        msg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
